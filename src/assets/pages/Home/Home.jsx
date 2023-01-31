39e17180c3b1342bc05df257dcf8aca21946d589
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-01-31 10:54:26 +0200

    Preparing

---
+++ src/assets/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div>Special Markings Are What We Do</div>
+      <Footer />
+    </>
+  );
+}
