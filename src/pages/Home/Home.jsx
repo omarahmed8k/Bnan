488e0d3a2b4ca61bd68d278566389ad9c2490940
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-02-06 23:52:39 +0200

    Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 import ReactTyped from "react-typed";
 import homeVideo from "../../assets/images/videos/video.mp4";
+import { ReactComponent as FixedLogo } from "../../assets/images/fixed-logo.svg";
+import { ReactComponent as ServiceImg1 } from "../../assets/images/services/1.svg";
+import { ReactComponent as ServiceImg2 } from "../../assets/images/services/2.svg";
+import { ReactComponent as ServiceImg3 } from "../../assets/images/services/3.svg";
+import { ReactComponent as ServiceImg4 } from "../../assets/images/services/4.svg";
 import "./Home.css";
 export default function Home() {
   const { t } = useTranslation();
@@ -32,6 +38,43 @@ export default function Home() {
               />
             </h1>
             <p>{t("home.background.desc")}</p>
+          </div>
+        </div>
+      </div>
+      <div className="services-section">
+        <FixedLogo className="fixed-logo" />
+        <div className="container">
+          <h2 className="title">{t("home.services.title")}</h2>
+          <p className="desc">{t("home.services.desc")}</p>
+          <div className="services-links">
+            <Link to={"/service/network"} className="services-card">
+              <div className="card-head">
+                <ServiceImg1 />
+                <h3>{t("home.services.card1Title")}</h3>
+              </div>
+              <p>{t("home.services.card1Desc")}</p>
+            </Link>
+            <Link to={"/service/network"} className="services-card">
+              <div className="card-head">
+                <ServiceImg2 />
+                <h3>{t("home.services.card2Title")}</h3>
+              </div>
+              <p>{t("home.services.card2Desc")}</p>
+            </Link>
+            <Link to={"/service/network"} className="services-card">
+              <div className="card-head">
+                <ServiceImg3 />
+                <h3>{t("home.services.card3Title")}</h3>
+              </div>
+              <p>{t("home.services.card3Desc")}</p>
+            </Link>
+            <Link to={"/service/network"} className="services-card">
+              <div className="card-head">
+                <ServiceImg4 />
+                <h3>{t("home.services.card4Title")}</h3>
+              </div>
+              <p>{t("home.services.card4Desc")}</p>
+            </Link>
           </div>
         </div>
       </div>
