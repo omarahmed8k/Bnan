ea0b3f84132e7bd67ddad24b78d1f2642b066a1c
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-02-11 00:18:00 +0200

    404

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import "./ErrorPage.css";
 
 export default function ErrorPage() {
@@ -6,12 +6,8 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" className="error-page">
+      <Link to={"/"}>Go to Home</Link>
     </div>
   );
 }
