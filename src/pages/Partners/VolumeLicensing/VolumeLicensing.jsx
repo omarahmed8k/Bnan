c9babfedd092ac80b2c3d97309faf2488f8d8e84
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-05-03 01:31:15 +0300

    Pages

--- src/pages/Partners/VolumeLicensing/VolumeLicensing.jsx
+++ src/pages/Partners/VolumeLicensing/VolumeLicensing.jsx
@@ -1,15 +1,35 @@
 import React from "react";
+import { Fade } from "react-reveal";
+import { useTranslation } from "react-i18next";
+import { ReactComponent as FixedLogo } from "../../../assets/images/svgs/fixed-logo.svg";
+import TopBackground from "../../../components/TopBackground/TopBackground";
+import ImageTextSection from "../../../components/ImageTextSection/ImageTextSection.jsx";
+import vision from "../../../assets/images/featured/featured1.jpg";
+import mission from "../../../assets/images/featured/featured2.jpg";
+import values from "../../../assets/images/featured/featured3.jpg";
 import "./VolumeLicensing.css";
-import ContentArea from "../../../components/ContentArea/ContentArea";
 
 function VolumeLicensing() {
+  const { t } = useTranslation();
   return (
     <div>
-      <ContentArea title="Volume Licensing" text="Our partners are the best!">
-        <p>Volume Licensing</p>
-      </ContentArea>
+      <TopBackground title="Volume Licensing" text="Our partners are the best!" />
+      <Fade bottom>
+        <div className="about-section">
+          <FixedLogo className="fixed-logo" />
+          <div className="container">
+            <h2 className="title">{t("Volume Licensing")}</h2>
+            <p className="desc">{t("about.desc")}</p>
+            <ImageTextSection type="row" title="about.about1Title" text="about.about1Desc" image={vision} />
+            <ImageTextSection type="row-reverse" title="about.about2Title" text="about.about2Desc" image={mission} />
+            <ImageTextSection type="row" title="about.about3Title" text="about.about3Desc" image={values} />
+            <ImageTextSection type="row-reverse" title="about.about2Title" text="about.about2Desc" image={mission} />
+          </div>
+        </div>
+      </Fade>
     </div>
   );
 }
+
 
 export default VolumeLicensing;
--- src/pages/Solutions/CoreInfrastructure/CoreInfrastructure.jsx
+++ src/pages/Solutions/CoreInfrastructure/CoreInfrastructure.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import { Fade } from "react-reveal";
+import { useTranslation } from "react-i18next";
+import { ReactComponent as FixedLogo } from "../../../assets/images/svgs/fixed-logo.svg";
+import TopBackground from "../../../components/TopBackground/TopBackground";
+import ImageTextSection from "../../../components/ImageTextSection/ImageTextSection.jsx";
+import vision from "../../../assets/images/featured/featured1.jpg";
+import mission from "../../../assets/images/featured/featured2.jpg";
+import values from "../../../assets/images/featured/featured3.jpg";
 import "./CoreInfrastructure.css";
-import ContentArea from "../../../components/ContentArea/ContentArea";
 
 function CoreInfrastructure() {
+  const { t } = useTranslation();
   return (
     <div>
-      <ContentArea
-        title="Core Infrastructure"
-        text="Our partners are the best!"
-      >
-        <p>Core Infrastructure</p>
-      </ContentArea>
+      <TopBackground title="Core Infrastructure" text="Our partners are the best!" />
+      <Fade bottom>
+        <div className="about-section">
+          <FixedLogo className="fixed-logo" />
+          <div className="container">
+            <h2 className="title">{t("Core Infrastructure")}</h2>
+            <p className="desc">{t("about.desc")}</p>
+            <ImageTextSection type="row" title="about.about1Title" text="about.about1Desc" image={vision} />
+            <ImageTextSection type="row-reverse" title="about.about2Title" text="about.about2Desc" image={mission} />
+            <ImageTextSection type="row" title="about.about3Title" text="about.about3Desc" image={values} />
+            <ImageTextSection type="row-reverse" title="about.about2Title" text="about.about2Desc" image={mission} />
+          </div>
+        </div>
+      </Fade>
     </div>
   );
 }
